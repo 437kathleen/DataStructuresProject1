200563dc9160d49bafa6d7b673bfe8afcab74c65
437kathleen <49416686+437kathleen@users.noreply.github.com>
2019-04-09 15:35:06 -0600

    User class
    initial setup

--- Functions.hpp
+++ Functions.hpp
@@ -1,1 +1,36 @@
-//.hpp file
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+
+using namespace std;
+
+struct transaction{
+  float amount;
+  float title;
+  transaction *next;
+};
+
+class User{
+  private:
+    string name;
+    float balance;
+    float debt;
+    transaction *historyHead;
+    string password;
+    void printPublicHistory();
+    void printPersonalHistory();
+
+  public:
+    User(string name);
+    ~User();
+    void getName();
+    void getBalance(bool tf);
+    void getDebt(bool tf);
+    void deposit(bool tf, int money);//check for negative amounts
+    void withdrawal(bool tf, int money);//check over draw
+    void printHistory(bool tf);//call either public or personal
+
+
+};
