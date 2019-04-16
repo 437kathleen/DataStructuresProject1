4cb5c7f1a24debfbaacbf3f7ad30ea402cd56d0e
437kathleen <49416686+437kathleen@users.noreply.github.com>
2019-04-16 17:13:57 -0600

    Coded 5 functions
    void User::loadTransactions(string fileName)
    void User::printPublicHistory()
    void User::printPersonalHistory()
    void User::loadUserInfo(string line)
    void User::addTransaction(string line)

--- classUser.cpp
+++ classUser.cpp
@@ -1,1 +1,90 @@
 //Functions
+#include <iostream>
+#include <fstream>
+//#include <sstream>
+#include <string>
+
+using namespace std;
+
+void User::loadTransactions(string fileName){
+  fstream myfile(fileName);
+  if(myfile.is_open()){
+    string line;
+    while(!myfile.eof()){
+      getline(myfile, line);
+      addTransaction(line);
+    }
+  }else{
+    cout<<"File could not be found"<<endl;
+  }
+}
+
+void User::printPublicHistory(){
+  if(historyHead == NULL){
+    cout<<"No history"<<endl;
+  }else{
+    transaction *temp = historyHead;
+    int i = 0;
+    while(temp!=NULL){
+      cout<<i<<": "<<temp.name<<endl;
+      temp = temp->next;
+    }
+  }
+}
+
+void User::printPersonalHistory(){
+  if(historyHead == NULL){
+    cout<<"No history"<<endl;
+  }else{
+    transaction *temp = historyHead;
+    int i = 0;
+    while(temp!=NULL){
+      cout<<setw(30)<<temp->name<<"$"<<setprecision(2)<<fixed<<temp->amount<<endl;
+      temp = temp->next;
+    }
+  }
+}
+
+
+void User::loadUserInfo(string line){
+  //File construct will be formatted like so
+  // name,password,balance,debt,(file name for transactions)
+  string item;
+  stringstream ss;
+  ss<<line;
+  getline(ss, item,',');
+  name = item;
+  getline(ss, item,',');
+  password = item;
+  getline(ss, item,',');
+  balance = stof(item);
+  getline(ss, item,',');
+  debt = stof(item);
+  getline(ss, item);
+  loadTransactions(item);
+}
+
+void User::addTransaction(string line){
+  //empty list
+  if(historyHead == NULL){
+    transaction *temp;
+    getline(ss, item,',');
+    temp.name = item;
+    getline(ss, item);
+    temp.amount = stof(item);
+    historyHead = temp;
+  }else{//traverse to last element and append on
+    transaction *temp = historyHead;
+    transaction *prev;
+    transaction *curr;
+    while(temp!=NULL){//append onto prev
+      prev = temp;
+      temp = temp->next;
+    }//temp == NULL
+    getline(ss, item,',');
+    curr.name = item;
+    getline(ss, item);
+    curr.amount = stof(item);
+    prev->next = curr;//add transaction to list
+  }
+}
