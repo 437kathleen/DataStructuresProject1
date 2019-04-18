285e24849236453043daf034c481b34b2a8ffc8e
437kathleen <49416686+437kathleen@users.noreply.github.com>
2019-04-17 23:38:43 -0600

    Need to code: 31-37
    Need To Implement
    User::~User();
    
    void User::getName();
    void User::getBalance(bool tf);
    void User::getDebt(bool tf);
    void User::deposit(bool tf, int money);//check for negative amounts
    void User::withdrawal(bool tf, int money);//check over draw
    void User::saveTransactions();//save into a file//check if filename is empty...if it is then it is a new user *make a file based on username*

--- classUser.hpp
+++ classUser.hpp
@@ -7,7 +7,7 @@ using namespace std;
 
 struct transaction{
   float amount;
-  float title;
+  string name;
   transaction *next;
 };
 
@@ -17,20 +17,25 @@ class User{
     string password;
     float balance;
     float debt;
+    User *next;
     transaction *historyHead;
-    
+    string transactionFile;
+  
     void printPublicHistory();
     void printPersonalHistory();
     void loadTransactions(string fileName);
 
   public:
-    User(string name);
+    User();
+  
     ~User();
     void getName();
     void getBalance(bool tf);
     void getDebt(bool tf);
     void deposit(bool tf, int money);//check for negative amounts
     void withdrawal(bool tf, int money);//check over draw
+    void saveTransactions();//save into a file
+  
     void printHistory(bool tf);//call either public or personal
     void loadUserInfo(string line);
     void addTransaction(string line);
