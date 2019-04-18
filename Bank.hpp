0ae253c988b1088a5f9805a1ac7152c3df549eaa
437kathleen <49416686+437kathleen@users.noreply.github.com>
2019-04-18 02:39:43 -0600

    Initial Commit

---
+++ Bank.hpp
@@ -0,0 +1,38 @@
+#ifndef BANK_HPP
+#define BANK_HPP
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include "User.hpp"
+
+using namespace std;
+
+class Bank{
+  private:
+    User** account;
+    int numAccounts;//unnecessary
+    int collisions;
+    int tableSize;//arbitary
+
+  public:
+    Bank(int tableSize);
+    ~Bank();
+
+    //int countLines(string myfile);//count lines in file
+    bool loadFile(string myfile);//
+
+    void saveFile(string myfile);//****************need to implement
+    User *getUser(string name, string password);
+
+    void addSavedUser(User *savedUser);
+    User *addUser(string name, string password);
+    bool isInTable(string name);
+    int findAscii(string name);
+    int getHash(string name);
+    int numCollisions();
+};
+
+#endif
